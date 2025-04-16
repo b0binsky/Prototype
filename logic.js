bdc001ad2d038a08962472e997d6f53a2f45b4a4
ReesaHronchek <rhronche@purdue.edu>
2025-04-16 00:29:06 -0400

    Did most of the CSS and updated JS

---
+++ logic.js
@@ -0,0 +1,21 @@
+points = 0;
+function correct(){
+    const messageBox = document.getElementById('message-box');
+    points += 1; 
+    messageBox.innerHTML = ' Good! you have ' + points + ' point(s)!';
+    messageBox.style.display = 'block';
+    setTimeout(() => { messageBox.style.display = '';}, 3000);
+}
+
+function wrong(){
+    const messageBox = document.getElementById('message-box'); 
+    points -= 1;
+    messageBox.innerHTML = ' Wrong :( you have ' + points + ' point(s).'; 
+    messageBox.style.display = 'block'; 
+    setTimeout(() => { messageBox.style.display = '';}, 3000);
+}
+
+function reset(){
+    points = 0;
+    messageBox.style.display = 'none';
+}
